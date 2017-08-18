f6794d4a85f179c6451c8d861be4ca123f4662e0
pamelarussell <pamela.russell@gmail.com>
2017-08-18 08:28:18 -0600

    use uniform style

--- tests/testthat/test_get_series_size.R
+++ tests/testthat/test_get_series_size.R
@@ -1,12 +1,8 @@
 message("\nTesting get_series_size")
 
-setup <- function() {
-  series_size <- get_series_size("1.3.6.1.4.1.14519.5.2.1.5382.4002.272234209223992578700978260744")
-}
-
 test_that("Structure of series size value", {
   skip_on_cran()
-  setup()
+  series_size <- get_series_size("1.3.6.1.4.1.14519.5.2.1.5382.4002.272234209223992578700978260744")
   expect_equal(length(series_size), 4)
   expect_true(series_size$size_bytes > 1000)
   expect_true(series_size$object_count > 10)
--- tests/testthat/test_get_sop_instance_uids.R
+++ tests/testthat/test_get_sop_instance_uids.R
@@ -1,13 +1,8 @@
 message("\nTesting get_sop_instance_uids")
 
-setup <- function() {
-  patients_tcga_mr <- get_patients_by_modality(collection = "TCGA-BRCA", modality = "MR")
-  uids <- get_sop_instance_uids("1.3.6.1.4.1.14519.5.2.1.5382.4002.806935685832642465081499816867")
-}
-
 test_that("Structure of SOPInstanceUIDs value", {
   skip_on_cran()
-  setup()
+  uids <- get_sop_instance_uids("1.3.6.1.4.1.14519.5.2.1.5382.4002.806935685832642465081499816867")
   expect_equal(length(uids), 3)
   expect_true(length(uids$sop_instance_uids) > 10)
   expect_true(length(uids$content) > 10)
@@ -16,7 +11,7 @@ test_that("Structure of SOPInstanceUIDs value", {
 
 test_that("Patient values for TCGA-BRCA and MR", {
   skip_on_cran()
-  setup()
+  uids <- get_sop_instance_uids("1.3.6.1.4.1.14519.5.2.1.5382.4002.806935685832642465081499816867")
   expect_true("1.3.6.1.4.1.14519.5.2.1.5382.4002.257663256941568276393774062283" %in% uids$sop_instance_uids)
 })
 
